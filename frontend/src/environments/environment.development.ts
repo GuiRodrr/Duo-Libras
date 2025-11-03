57100b4c7a48f7665f2deb08d56bc91664802a7a
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2025-11-03 23:44:16 +0000

    [Frontend] fix: rota

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiMemberCodespace: 'https://stunning-space-guacamole-jxjwxxqq75wcqp97-4200.app.github.dev/member/', // necessário para o uso no codespace: apagar e usar apenas localhost
-  apiMember: 'http://localhost:5000/member/',
+  // apiMemberCodespace: 'https://stunning-space-guacamole-jxjwxxqq75wcqp97-4200.app.github.dev/member/', // necessário para o uso no codespace: apagar e usar apenas localhost
+  apiMember: 'https://duo-libras-production.up.railway.app/member/'
 };
